50e92076208a5bf07ddc241f68d54ac6c8a667c4
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-14 18:54:09 +0545

    learn about data types

---
+++ 01_Basics/dataTypes.py
@@ -0,0 +1,26 @@
+# Data Types in python
+# int
+# float
+# None
+# bool
+# str
+# tuple
+# set
+# dict
+
+x = 30
+y = 9.0
+name = "narendra"
+isSingle = True
+subjects = ["C#", "CDC"]  # list
+userInfo = {"name": "John", "age": 36}  # dict
+admin = None
+
+
+print(type(x))
+print(type(y))
+print(type(isSingle))
+print(type(subjects))
+print(type(userInfo))
+print(type(admin))
+print(type(name))
