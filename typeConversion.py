29a160da61d6c126ee060a7fcf1f39b08735602b
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-14 19:07:17 +0545

    learn about type conversion

---
+++ typeConversion.py
@@ -0,0 +1,15 @@
+# Type Casting is the method to convert the Python variable datatype into a certain data type in order to perform the required operation by users.
+
+x = 8
+y = 9.0
+z = x + y  # Implicit
+print(type(z))
+
+
+# Explicit
+
+
+b = float(29)
+c = str(b)
+print(type(b))
+print(type(c))
